6a7c8e97adb14ce9a2b8055701188886c1d7383e
Dazai <dazai1317@mail.ru>
2024-03-06 01:03:31 +0700

    визуализация

--- close.js
+++ close.js
@@ -15,3 +15,25 @@ function importXml() {
     var exportPopup = document.getElementById("exportPopup");
     exportPopup.style.display = "none";
   }
+  function openImportPopup() {
+    var importPopup = document.getElementById("importPopup");
+    var overlay = document.getElementById("overlay");
+    importPopup.style.display = "block";
+    overlay.style.display = "block";
+  }
+  
+  function openExportPopup() {
+    var exportPopup = document.getElementById("exportPopup");
+    var overlay = document.getElementById("overlay");
+    exportPopup.style.display = "block";
+    overlay.style.display = "block";
+  }
+  
+  function closePopup() {
+    var importPopup = document.getElementById("importPopup");
+    var exportPopup = document.getElementById("exportPopup");
+    var overlay = document.getElementById("overlay");
+    importPopup.style.display = "none";
+    exportPopup.style.display = "none";
+    overlay.style.display = "none";
+  }
