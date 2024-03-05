c691e5b10851b7577097ea893dddcac12b533203
Dazai <dazai1317@mail.ru>
2024-03-06 00:57:05 +0700

    визуализация

---
+++ close.js
@@ -0,0 +1,17 @@
+function importXml() {
+    // Код для обработки импорта XML-файлов
+  }
+  
+  function cancelImport() {
+    var importPopup = document.getElementById("importPopup");
+    importPopup.style.display = "none";
+  }
+  
+  function exportXml() {
+    // Код для обработки экспорта XML-файлов
+  }
+  
+  function cancelExport() {
+    var exportPopup = document.getElementById("exportPopup");
+    exportPopup.style.display = "none";
+  }
